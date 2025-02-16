f31681a454b3ad2906b915cc4698a139f7ff10e0
GlitchBoyopyt <162595849+GlitchBoyopyt@users.noreply.github.com>
2025-02-17 00:36:38 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!hello') {
+    message.channel.send("Hello, I'm your bot!");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
